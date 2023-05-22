4ceba0bc0a6551323991da5bfb55edc4c4fb419c
FahadHasan8386 <fh223430@gmail.com>
2023-05-22 22:59:56 +0600

    Beecrowd problem Solving -1010

--- uri1010.cpp
+++ uri1010.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main ()
-{
-    int code1 ,units1 ,code2 ,units2;
-    float price1 ,price2;
-        cin >>code1 >>units1 >>price1;
-        cin >>code2 >>units2 >>price2;
 
-    float result = (price1*units1) + (price2*units2);
-    cout << fixed ;
-    cout <<setprecision (2)<<"VALOR A PAGAR: R$ " <<result  <<endl;
+int main() {
+    int code1, units1, code2, units2;
+    float price1, price2;
+        cin>>code1 >>units1 >>price1;
+        cin>>code2 >>units2 >>price2;
+
+
+    float total = (units1 * price1) + (units2 * price2);
+
+        cout<< fixed;
+        cout <<setprecision(2)<< "VALOR A PAGAR: R$ " << total<<endl;
 }
