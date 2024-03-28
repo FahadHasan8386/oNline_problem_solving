0eda104a2f7d34cb943cb6a54cd1714fd59c4333
fahadhasan <hasan.fahad67@outlook.com>
2024-03-29 00:51:31 +0600

    Code Forces Problem 61A

--- cf61.cpp
+++ cf61.cpp
@@ -6,11 +6,12 @@ int main (){
         cin >> num1 >> num2;
     for (int i = 0 ; i < num1.length() ; i++)
     {
-        if (num1[i] == '1' || num2[i] == '1')
+        if (num1[i] == '0' && num2[i] == '0' ||  num1[i] == '1' && num2[i] == '1')
         {
-            cout << "1";
-        }else {
             cout << "0";
+        }
+        else{
+            cout << "1";
         }
     }
 }
