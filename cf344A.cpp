9270d5f3c0cca9795870414b6b09d2f3a51e99e8
fahadhasan <hasan.fahad67@outlook.com>
2024-03-30 00:34:52 +0600

    Code forces Problem 148A

--- cf344A.cpp
+++ cf344A.cpp
@@ -5,15 +5,16 @@ int main ()
     int sz;
         cin >> sz;
     int num1[sz];
-
+    int counter = 0;
     for (int i = 0; i < sz ; i++)
     {
         cin >> num1[i];
-        if (num1[i] != num1[i + 1])
-        {
-            cout << num1[i];
-
+    }
+    for (int i = 0 ; i < sz ; i++)
+    {
+        if(num1[i] != num1[i + 1]){
+            counter++;
         }
     }
-return 0;
+    cout << counter;
 }
