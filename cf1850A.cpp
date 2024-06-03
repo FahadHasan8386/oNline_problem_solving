8a8c4533c29815d01dd2d6a033bf5223de3132a6
fahadhasan <hasan.fahad67@outlook.com>
2024-06-03 11:52:22 +0600

    Code Forces Problem 1850A

---
+++ cf1850A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int t ;
+    cin >> t ;
+
+    while (t--)
+    {
+        int a ,b , c;
+        cin >> a >> b >> c ;
+
+        int sum1 = a + b;
+        int sum2 = a + c;
+        int sum3 = b + c;
+        if(sum1 >= 10 || sum2 >= 10 || sum3 >= 10)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
