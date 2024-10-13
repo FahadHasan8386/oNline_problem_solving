cd98a4bbd33d425d843e46bb01a48a90a049ca90
Fahad Hasan <hasan.fahad67@outlook.com>
2024-10-13 22:39:41 +0600

    Code Forces Problem 1928A

---
+++ cf1928A.cpp
@@ -0,0 +1,35 @@
+///IN the name of Allah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if(a < b )
+        {
+            swap(a,b);
+        }
+        if(b % 2 == 1)
+        {
+            if(a % 2 == 0 && a != 2*b)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+}
