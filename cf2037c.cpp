dc0cf2b2c4788b7858267e4acb1dc387f1fc1e5b
Fahad Hasan <hasan.fahad67@outlook.com>
2024-11-20 23:09:13 +0600

    Code Forces Problem 2037C

--- cf2037c.cpp
+++ cf2037c.cpp
@@ -3,26 +3,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
+void solve()
+{
     int t;
     cin >> t;
 
-    while (t--) {
-        int n;
+    while (t--)
+    {
+        long long n;
         cin >> n;
 
-        if (n == 3) {
+        if (n <= 4)
+        {
             cout << -1 << "\n";
-        } else {
-            for (int i = n; i >= 1; i--) {
-                cout << i << " ";
+        }
+        else
+        {
+            for (int i = 1; i <= n; i+=2)
+            {
+                if(i != 5)
+                {
+                    cout << i << " ";
+                }
+
             }
-            cout << "\n";
+            cout << 5 << " " << 4 << " ";
+            for (int i = 2; i <= n; i+=2)
+            {
+                if(i != 4)
+                {
+                    cout << i << " ";
+                }
+
+            }
+            cout << endl;
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
