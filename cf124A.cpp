f654911ca53a233acf4f2b66bc69f0818bc9f49e
Fahad Hasan <hasan.fahad67@outlook.com>
2024-12-25 20:58:29 +0600

    cf124A

---
+++ cf124A.cpp
@@ -0,0 +1,18 @@
+///In the name of Allah
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main ()
+{
+    int n , a , b;
+    cin >> n >> a >> b;
+
+    if((n - a) > (b + 1))
+    {
+        cout << b+1 << endl;
+    }
+    else{
+        cout << n - a << endl;
+    }
+}
