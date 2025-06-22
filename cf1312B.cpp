5680b2510be5e8b91684233e4d03f567f5038fd9
Fahad Hasan <hasan.fahad67@outlook.com>
2025-06-22 14:39:47 +0600

    Cf 1312B

---
+++ cf1312B.cpp
@@ -0,0 +1,28 @@
+///In the name of Allah
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main ()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int>v(n);
+        for(int i  = 0 ; i < n ;i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.rbegin(),v.rend());
+        for(int i = 0 ; i < n ;i++)
+
+        {
+            cout << v[i] << " " ;
+        }
+        cout << endl;
+    }
+}
