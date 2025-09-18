dc36d3bffef39262add0a2a531e74e2f4ef9c206
Fahad Hasan <hasan.fahad67@outlook.com>
2025-09-18 10:02:27 +0600

    CF Binary search ,Greedy

--- cf2065c1.cpp
+++ cf2065c1.cpp
@@ -7,7 +7,35 @@ void fahad()
     int n , m;
     cin >> n >> m;
 
-    vector<int>v(n),v1(m);
+     vector<long long> a(n), b(m);
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    long long B = b[0];
+    long long prev = -1e18;
+
+     for (int i = 0; i < n; i++) {
+        long long keep = a[i];
+
+        long long change = B - a[i];
+        long long choice = 1e18;
+
+        if (keep >= prev) choice = min(choice, keep);
+
+
+        if (change >= prev) choice = min(choice, change);
+
+
+        if (choice == 1e18) {
+            cout << "NO" << endl;
+            return;
+        }
+
+        prev = choice;
+    }
+cout << "YES" << endl;
+
 }
 
 int main ()
