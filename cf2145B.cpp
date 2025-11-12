a78964bdc9cf91b43caca62a97a79b0c523bc31d
Fahad Hasan <hasan.fahad67@outlook.com>
2025-11-12 09:30:45 +0600

    Cf2145

---
+++ cf2145B.cpp
@@ -0,0 +1,35 @@
+///IN the name of ALLAH
+#include <bits/stdc++.h>
+using namespace std;
+
+void fahad()
+{
+    int n, k;
+    string s;
+    cin >> n >> k >> s;
+
+    int a = count(s.begin(),s.end() , '0');
+    int b = count(s.begin(),s.end() , '1');
+    int c = count(s.begin(),s.end() , '2');
+
+    string  ans(n ,'+');
+
+    for(int i = 0 ; i < n ;i++)
+    {
+        if(i < a + c || i >= n - b - c) ans[i] = '?';
+        if(i < a || i >=  n - b || k == n) ans[i] = '-';
+    }
+    cout << ans << endl;
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        fahad();
+    }
+}
